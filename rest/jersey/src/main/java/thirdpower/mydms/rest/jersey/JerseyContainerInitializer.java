7065b467eea2ccc7e6314132f5a2b986076e6d30
MtTracer <MtTracer@users.noreply.github.com>
2017-08-30 08:05:26 +0200

    fix jersey servlet initializer

--- rest/jersey/src/main/java/thirdpower/mydms/rest/jersey/JerseyContainerInitializer.java
+++ rest/jersey/src/main/java/thirdpower/mydms/rest/jersey/JerseyContainerInitializer.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
-import javax.ws.rs.core.Application;
 
 import org.glassfish.jersey.servlet.ServletContainer;
 
@@ -18,7 +17,8 @@ public class JerseyContainerInitializer implements ServletContainerInitializer {
   @Override
   public void onStartup(final Set<Class<?>> c, final ServletContext ctx) throws ServletException {
     final Dynamic servletConfig = ctx.addServlet("JerseyServlet", ServletContainer.class);
-    servletConfig.setInitParameter(Application.class.getName(), MyDmsJerseyConfig.class.getName());
+    servletConfig.setInitParameter("javax.ws.rs.Application", MyDmsJerseyConfig.class.getName());
+    servletConfig.setLoadOnStartup(1);
     servletConfig.addMapping("/rest/*");
 
   }
--- rest/jersey/src/main/java/thirdpower/mydms/rest/jersey/MyDmsJerseyConfig.java
+++ rest/jersey/src/main/java/thirdpower/mydms/rest/jersey/MyDmsJerseyConfig.java
@@ -2,9 +2,10 @@ package thirdpower.mydms.rest.jersey;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+
 public class MyDmsJerseyConfig extends ResourceConfig {
 
-  MyDmsJerseyConfig() {
+  public MyDmsJerseyConfig() {
     packages(true, "thirdpower.mydms");
   }
 
