a2999cf8a6b098e456b7198f5774d5cd4d1d2116
MtTracer <mttracer@server.fake>
2017-08-30 16:57:53 +0200

    clean poms and SanityTests

--- persistence/h2embedded/src/main/java/thirdpower/mydms/persistence/h2embedded/GuicePersistListener.java
+++ persistence/h2embedded/src/main/java/thirdpower/mydms/persistence/h2embedded/GuicePersistListener.java
@@ -1,5 +1,7 @@
 package thirdpower.mydms.persistence.h2embedded;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -13,12 +15,14 @@ public class GuicePersistListener implements ServletContextListener {
 
   @Override
   public void contextInitialized(ServletContextEvent sce) {
-     PersistService persistService = DependencyFactory.getInstance(PersistService.class);
-     persistService.start();
+    checkNotNull(sce);
+    PersistService persistService = DependencyFactory.getInstance(PersistService.class);
+    persistService.start();
   }
-  
+
   @Override
   public void contextDestroyed(ServletContextEvent sce) {
+    checkNotNull(sce);
     PersistService persistService = DependencyFactory.getInstance(PersistService.class);
     persistService.stop();
   }
